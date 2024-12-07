0f69e15a85cf3f418e284d161d5084956b1ea4d7
Ian Liu <79739194+ian420602@users.noreply.github.com>
2024-12-06 20:19:53 -0800

    add Json class(templet)

---
+++ PA4/Class_Library/JsonOutput.cs
@@ -0,0 +1,15 @@
+namespace Class_Library;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+
+public class JSONOutput {
+    public JSONOutput()
+    {
+        Console.WriteLine("Hello, World!");
+    }  
+}
+
